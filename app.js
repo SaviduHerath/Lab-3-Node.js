dd0162eb9c2fcb9c4cec4f32e06db2f8198bf2f9
Savidu Herath <saviduherath2003@gmail.com>
2026-02-22 23:58:01 +0530

    Promises

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const fs = require('fs');
-fs.readFile('file.txt', 'utf8', function (err, data) {
- if (err) throw err;
- console.log(data);
+const myPromise = new Promise((resolve, reject) => {
+ if (condition) {
+ resolve('Success!');
+ } else {
+ reject('Failure!');
+ }
+});
+myPromise.then((result) => {
+ console.log(result);
+}).catch((error) => {
+ console.log(error);
 });
